e7e59a242a053ea8c2d0c6ad2c0ebfb0f5d263de
mike <mjbutt@hotmail.co.uk>
2017-11-09 23:43:07 +0000

    Initial Check in

---
+++ Pip-Boy-MVC/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pip_Boy_MVC.Models;
+
+namespace Pip_Boy_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.experience = "Level 22";
+            ViewBag.health = "HP 345/360";
+            ViewBag.action = "AP 123/345";
+            return View();
+        }
+    }
+}
---
+++ Pip-Boy-MVC/wwwroot/js/site.js
@@ -0,0 +1,44 @@
+﻿function resetNav() {
+    document.getElementById("stat-nav").className = "stat";
+    document.getElementById("inv-nav").className = "inv";
+    document.getElementById("data-nav").className = "data";
+    document.getElementById("radio-nav").className = "radio";
+    document.getElementById("map-nav").className = "map";
+    document.getElementById("status-nav").className = "status";
+
+    document.getElementById("vb-radio").className = "vaultboy-hidden";
+    document.getElementById("vb-stat").className = "vaultboy-hidden";
+};
+
+function invPage() {
+    resetNav();
+    document.getElementById("inv-nav").className = "active inv";
+};
+
+function statPage() {
+    resetNav();
+    document.getElementById("stat-nav").className = "active stat";
+    document.getElementById("vb-stat").className = "vaultboy";
+};
+
+function dataPage() {
+    resetNav();
+    document.getElementById("data-nav").className = "active data";
+};
+
+function radioPage() {
+    resetNav();
+    document.getElementById("radio-nav").className = "active radio";
+};
+
+function mapPage() {
+    resetNav();
+    document.getElementById("map-nav").className = "active map";
+};
+
+function radioPage() {
+    resetNav();
+    document.getElementById("radio-nav").className = "active radio";
+    document.getElementById("vb-radio").className = "vaultboy";
+    
+};
